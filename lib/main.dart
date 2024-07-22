eefaf9a09e6b3374973098e9ce71619c5cf7b3aa
aenulfahir03 <aenulfahir03@gmail.com>
2024-07-22 07:03:25 +0000

    add buyers file

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
+        fontFamily: 'Brand-Bold'
       ),
       home: MainScreen(),
     );
--- lib/views/buyers/main_screen.dart
+++ lib/views/buyers/main_screen.dart
@@ -1,9 +1,12 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:myapp/views/buyers/nav_screens/account_screen.dart';
+import 'package:myapp/views/buyers/nav_screens/cart_screen.dart';
+import 'package:myapp/views/buyers/nav_screens/category_screen.dart';
+import 'package:myapp/views/buyers/nav_screens/home_screen.dart';
+import 'package:myapp/views/buyers/nav_screens/search_screen.dart';
+import 'package:myapp/views/buyers/nav_screens/store_screen.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -13,20 +16,59 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int _pageIndex = 0;
+
+  List<Widget> _pages = [
+    HomeScreen(),
+    CategoryScreen(),
+    StoreScreen(),
+    CartScreen(),
+    SearchScreen(),
+    AccountScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
-        unselectedItemColor: Colors.black,
-        selectedItemColor: Colors.yellow.shade900,
-        items: [
-        BottomNavigationBarItem(icon: Icon(CupertinoIcons.home),label: 'Home',),
-        BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/explore.svg'),label: 'Categories',),
-        BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/shop.svg'),label: 'Store',),
-        BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/cart.svg'),label: 'Cart',),
-        BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/search.svg'),label: 'Search',),
-        BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/account.svg'),label: 'Account',),
-        ]),
+          currentIndex: _pageIndex,
+          onTap: (value) {
+            setState(() {
+              _pageIndex = value;
+            });
+          },
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.white,
+          unselectedItemColor: Colors.black,
+          selectedItemColor: Colors.yellow.shade900,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/icons/explore.svg'),
+              label: 'Categories',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/icons/shop.svg'),
+              label: 'Store',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/icons/cart.svg'),
+              label: 'Cart',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/icons/search.svg'),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/icons/account.svg'),
+              label: 'Account',
+            ),
+          ],
+        ),
+        body: _pages[_pageIndex],
     );
   }
 }
