61fb78a31831725034fc6d8fe073a39291364ccb
aenulfahir03 <aenulfahir03@gmail.com>
2024-07-22 06:41:14 +0000

    Update main_screen.dart main.dart

--- lib/views/buyers/main_screen.dart
+++ lib/views/buyers/main_screen.dart
@@ -1,7 +1,9 @@
 
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/svg.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -13,8 +15,18 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center (child: Text('Main Screen'),)
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.yellow.shade900,
+        items: [
+        BottomNavigationBarItem(icon: Icon(CupertinoIcons.home),label: 'Home',),
+        BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/explore.svg'),label: 'Categories',),
+        BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/shop.svg'),label: 'Store',),
+        BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/cart.svg'),label: 'Cart',),
+        BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/search.svg'),label: 'Search',),
+        BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/account.svg'),label: 'Account',),
+        ]),
     );
   }
 }
