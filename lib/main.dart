83ead9490a9d8f383fddbc81a29de241a8ac2a0a
aenulfahir03 <aenulfahir03@gmail.com>
2024-07-27 05:14:39 +0000

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:myapp/views/buyers/main_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+              apiKey: 'AIzaSyBqzyeHXf361YZ1tUImRc1qpBM9f3SjyuI',
+              appId: '1:520442974585:android:8f7b72f4fda0da219f9fa8',
+              messagingSenderId: '520442974585',
+              projectId: 'port-store-79f96',
+              storageBucket: 'gs://port-store-79f96.appspot.com'
+              ))
+      : await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -12,16 +26,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: 'Brand-Bold'
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Brand-Bold'),
       home: MainScreen(),
     );
   }
 }
-
